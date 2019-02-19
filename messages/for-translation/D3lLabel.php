a6417d00ea578ef232b097c724be9028994519b1
Lauris Mierkalns <latpages@gmail.com>
2019-02-19 11:47:38 +0200

    Tulkojumi

---
+++ messages/for-translation/D3lLabel.php
@@ -0,0 +1,6 @@
+<?php
+if(false){
+    echo Yii::t('d3labels', 'D3labelsD3lLabelFull');
+    echo Yii::t('d3labels', 'D3labelsD3lLabelView');
+    echo Yii::t('d3labels', 'D3labelsD3lLabelEdit');
+}
