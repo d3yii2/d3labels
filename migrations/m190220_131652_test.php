cc8a50a1e23781373520a807a408ddb17a47d7ce
Lauris Mierkalns <latpages@gmail.com>
2019-02-22 18:48:50 +0200

    [Test] Migrācijas piemērs

--- migrations/m190220_131652_test.php
+++ migrations/m190220_131652_test.php
@@ -14,14 +14,12 @@ class m190220_131652_test extends Migration
         $items = [
             [
                 'modelClass' => 'yii2d3\d3emails\models\D3pop3Email',
-                'actionClass' => 'yii2d3\d3emails\controllers\EmailController',
                 'label' => ['title' => 'Testa labelis', 'icon' => 'icon'],
-                'sysCompany' => 1,
             ]
         ];
 
         foreach ($items as $label) {
-            $def = new \d3yii2\d3labels\logic\D3Definition($label['modelClass'], $label['actionClass'], $label['label'], $label['sysCompany']);
+            $def = new \d3yii2\d3labels\logic\D3Definition($label['modelClass'], $label['label']);
             $def->save();
         }
 
