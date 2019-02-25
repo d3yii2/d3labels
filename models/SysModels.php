76b8f323e2499aac4fe4d15f54a45b8f544f4724
Lauris Mierkalns <latpages@gmail.com>
2019-02-25 15:52:41 +0200

    SysModels komentāra labojums

--- models/SysModels.php
+++ models/SysModels.php
@@ -3,7 +3,7 @@
 namespace d3yii2\d3labels\models;
 
 /**
- * This is the model class for table "d3l_label".
+ * This is the model class for table "sys_models".
  */
 class SysModels extends \d3system\models\SysModels
 {
