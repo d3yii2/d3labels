2a504ad57370966d27c533c2b98fe3e2132340a8
Lauris Mierkalns <latpages@gmail.com>
2019-03-04 08:54:43 +0200

    D3lLabel - jauna metode getAllByModelRecordIds

--- models/D3lLabel.php
+++ models/D3lLabel.php
@@ -2,6 +2,7 @@
 
 namespace d3yii2\d3labels\models;
 
+use d3yii2\d3labels\models\D3lDefinition;
 use Yii;
 use \d3yii2\d3labels\models\base\D3lLabel as BaseD3lLabel;
 use yii\helpers\ArrayHelper;
@@ -30,5 +31,21 @@ public function behaviors()
                   # custom validation rules
              ]
         );
+    }
+
+    public static function getAllByModelRecordIds(array $ids)
+    {
+        if(empty($ids)) {
+            return [];
+        }
+
+        $models = (new \yii\db\Query())
+            ->select('*')
+            ->leftJoin(D3lDefinition::tableName(), self::tableName() . '.definition_id = ' . D3lDefinition::tableName() . '.id')
+            ->from(self::tableName())
+            ->where(['model_record_id' => $ids])
+            ->all();
+
+        return $models;
     }
 }
