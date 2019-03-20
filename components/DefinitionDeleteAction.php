111da346d1ef29b8b5187941bfa3ba384f50f4b7
Lauris Mierkalns <latpages@gmail.com>
2019-03-20 21:26:24 +0200

    Birku dzēšana - DefinitionDeleteAction klase

---
+++ components/DefinitionDeleteAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace d3yii2\d3labels\components;
+
+use d3yii2\d3labels\logic\D3Definition;
+use d3yii2\d3labels\logic\D3Label;
+use d3yii2\d3labels\models\D3lDefinition;
+use eaBlankonThema\components\FlashHelper;
+use Yii;
+
+/**
+ * Class DeleteAction
+ * Deletes an existing Label attached to Model
+ * @package d3yii2\d3labels\components
+ */
+class DefinitionDeleteAction extends BaseAction
+{
+
+    /**
+     */
+    public function run(int $modelId, int $definitionId): yii\web\Response
+    {
+        try {
+            $model = D3lDefinition::findOne(['id' => $definitionId, 'model_id' => $modelId]);
+
+            $hasLabels = $model->d3lLabels;
+
+            if ($hasLabels) {
+                FlashHelper::addWarning(Yii::t('d3labels','Cannot delete.  Attached to records'));
+            } elseif (!$model || !$model->delete()) {
+                FlashHelper::addWarning(Yii::t('d3labels','Not exists or deleted already'));
+            } else {
+
+                $msg = Yii::t('d3labels', 'Deleted sucessfully');
+                FlashHelper::addSuccess($msg);
+            }
+        } catch (\Exception $err) {
+            FlashHelper::addDanger($err->getMessage());
+        }
+
+        return $this->redirect();
+    }
+}
